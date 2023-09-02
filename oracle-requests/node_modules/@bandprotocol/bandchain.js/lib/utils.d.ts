ac2544a80337030aba703dd5f566c4044c2bb3f1
Nguyen Van Trinh <nguyenvantrinh2607@gmail.com>
2023-09-02 10:16:52 +0700

    update: runtime

--- oracle-requests/node_modules/@bandprotocol/bandchain.js/lib/utils.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function sortAndStringify(obj: any): string;
--- oracle-scripts/node_modules/@bandprotocol/bandchain.js/lib/constant.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const MAX_CLIENT_ID_LENGTH = 128;
-export declare const MAX_DATA_SIZE = 256;
-export declare const MAX_MEMO_CHARACTERS = 256;
